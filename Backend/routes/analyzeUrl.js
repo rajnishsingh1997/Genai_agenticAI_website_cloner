d06eac7cbc706fbd88441f5dbaeaf735e2e5f4b0
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-09-23 17:45:19 +0530

    added axios

--- Backend/routes/analyzeUrl.js
+++ Backend/routes/analyzeUrl.js
@@ -2,11 +2,11 @@ import express from "express";
 import isValidURL from "../utils/validateUrl.js";
 import axios from "axios";
 
+
 const analyzeUrl = express();
 
 analyzeUrl.post("/analyze", async (req, res) => {
   const { url } = req.body;
-
   if (!url) {
     return res.status(400).json({
       success: false,
@@ -22,12 +22,11 @@ analyzeUrl.post("/analyze", async (req, res) => {
       message: "Provided Url is not valid",
     });
   }
+  
   const response = await axios.get(url);
-
-
   res.status(200).json({
     success: true,
-    message: "message",
+    message: response.data,
   });
 });
 
