e422623b21ef63710d11ae8833db57fd03cd5fe9
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-09-26 05:41:26 +0530

    adding service file

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import analyzeApi from "./routes/analyzeUrl.js";
 dotenv.config();
-
 // import statement above this line
 
 const app = express();
 const portNumber = process.env.PORT;
+
 app.use(express.json());
 app.use("/api", analyzeApi);
+
+
+
+
 app.listen(portNumber, () => {
   console.log(`server running on port:${portNumber}`);
 });
